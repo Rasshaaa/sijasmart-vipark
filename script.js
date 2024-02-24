fb1672abbd51ccd837c7bf3e022cfe92a0640765
Rasshaaa <maulanarasya264@gmail.com>
2024-02-24 12:05:28 +0700

    Added Animation and font-resize for different breakpoints

--- script.js
+++ script.js
@@ -14,13 +14,36 @@ window.addEventListener('load', function() {
 
     // Initialize AOS after preloader finishes
     AOS.init();
-  }, 7000); // Example: 3000 milliseconds (3 seconds)
+  }, 6000); // Example: 3000 milliseconds (3 seconds)
 });
 
-var rellax = new Rellax('.rellax');
+var rellax = new Rellax('.rellax', {
+  breakpoints: [576, 768, 1201]
+});
 
 let year = document.querySelector("#year");
 
-$(document).ready(function () {
+$(year).ready(function () {
   year.innerText = new Date().getFullYear();
+});
+
+let custom_scroll = document.getElementById('body');
+let scroll_timeout = undefined;
+let ratio = 0.0000001;//change this to increase/descrease scroll speed/step
+custom_scroll.addEventListener("wheel", (event) => {
+  passive: false;
+  event.preventDefault();
+
+    clearTimeout(scroll_timeout);//if the previous scroll didn't finish, need to stop it to prevent infinite scroll.
+
+    let target = custom_scroll.scrollTop + event.deltaY * ratio;
+    let step = target > custom_scroll.scrollTop ? 1 : -1;
+
+    let frame = () => {
+        custom_scroll.scrollTop += step;
+        if((target < custom_scroll.scrollTop && step < 0) || (target > custom_scroll.scrollTop && step > 0)) {
+          scroll_timeout = setTimeout(frame, 5);
+        }
+    }
+    frame();
 });
