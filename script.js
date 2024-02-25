e87af5b694ca7421aae122eb77baa9421e87d19d
Rasshaaa <maulanarasya264@gmail.com>
2024-02-25 14:25:46 +0700

    Home Section fix Responsive

--- script.js
+++ script.js
@@ -26,24 +26,3 @@ let year = document.querySelector("#year");
 $(year).ready(function () {
   year.innerText = new Date().getFullYear();
 });
-
-let custom_scroll = document.getElementById('body');
-let scroll_timeout = undefined;
-let ratio = 0.0000001;//change this to increase/descrease scroll speed/step
-custom_scroll.addEventListener("wheel", (event) => {
-  passive: false;
-  event.preventDefault();
-
-    clearTimeout(scroll_timeout);//if the previous scroll didn't finish, need to stop it to prevent infinite scroll.
-
-    let target = custom_scroll.scrollTop + event.deltaY * ratio;
-    let step = target > custom_scroll.scrollTop ? 1 : -1;
-
-    let frame = () => {
-        custom_scroll.scrollTop += step;
-        if((target < custom_scroll.scrollTop && step < 0) || (target > custom_scroll.scrollTop && step > 0)) {
-          scroll_timeout = setTimeout(frame, 5);
-        }
-    }
-    frame();
-});
